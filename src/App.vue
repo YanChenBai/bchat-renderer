e05aaed217ca32058995619e73c1e9e01a0d2cce
BaiYanchen <2786660867@qq.com>
2025-07-14 14:53:54 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts" vapor>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function add() {
+  count.value++
+}
+</script>
+
+<template>
+  <button @click="add">
+    {{ count }}
+  </button>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createVaporApp } from 'vue'
+import App from './App.vue'
+import './assets/main.css'
+
+createVaporApp(App).mount('#app')
